38a911303d38cd0c68a3447c2686dc24d178cf78
AlexandrReznik <saha.reznik2004@gmail.com>
2023-03-01 14:03:16 +0200

    .

--- blog/models/user.py
+++ blog/models/user.py
@@ -5,7 +5,9 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
-    username = Column(String(80), nullable=False)
+    username = Column(String(80), unique=True, nullable=False)
+    email = Column(String(255), nullable=False, default="",
+                   server_default="", unique=True)
     is_staff = Column(Boolean, nullable=False, default=False)
 
     def __repr__(self):
