7ca1b36b7f84946c9ba742058fc40959a2611ccc
AlexandrReznik <saha.reznik2004@gmail.com>
2023-03-18 14:52:26 +0200

    Interface updated

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -1,9 +1,9 @@
-from flask_combo_jsonapi import Api
 from blog.api.tag import TagList, TagDetail
 from combojsonapi.spec import ApiSpecPlugin
-from blog.api.user import UserList, UserDetail
-from blog.api.author import AuthorList, AuthorDetail
-from blog.api.article import ArticleList, ArticleDetail
+from user import UserList, UserDetail
+from author import AuthorList, AuthorDetail
+from article import ArticleList, ArticleDetail
+from flask_combo_jsonapi import Api
 
 
 def create_api_spec_plugin(app):
--- blog/schemas/__init__.py
+++ blog/schemas/__init__.py
@@ -1,7 +1,8 @@
-from blog.schemas.tag import TagSchema
-from blog.schemas.user import UserSchema
-from blog.schemas.author import AuthorSchema
-from blog.schemas.article import ArticleSchema
+from tag import TagSchema
+from user import UserSchema
+from author import AuthorSchema
+from article import ArticleSchema
+
 
 __all__ = [
     "TagSchema",
